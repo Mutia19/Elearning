584767d46d731d5941c6d22746c363499e3de6c8
Mutia19 <zaa.mutia01@gmail.com>
2024-01-31 15:14:16 +0700

    Commit ke 2

--- Test Suites/Tes Login.ts
+++ Test Suites/Tes Login.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Upload</name>
+   <name>Tes Login</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,19 +10,12 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>b861a517-6b14-4f3f-b2c8-acebb50b5df4</testSuiteGuid>
+   <testSuiteGuid>7e140e01-76aa-47cc-a9b7-f2b9e6a2f050</testSuiteGuid>
    <testCaseLink>
-      <guid>36b39021-460c-4111-a7f3-3b1e29ea9f25</guid>
+      <guid>20137ed0-e173-4c47-afbf-c21c8daac835</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>ac711e39-0058-4f22-8fd7-851c467e916f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Upload</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
